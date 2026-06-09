b70f30947fa8398631bd571209eb0ac681866be1
Gib <agivens1337@gmail.com>
2026-06-09 19:11:41 -0400

    Refactor script.js to add calculator functions
    Added functions necessary for calculator. added a display so you could see what calculator was doing.TODO: more functions to add like tan sin and cosin, other buttons.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function goToPage2() {
     window.location.href = "page2.html";
 }
+
+function press(value) {
+    document.getElementById("display").value += value;
+}
+
+function cleardisplay() {
+    document.getElementById("display").value = "";
+}
+function calculate() {
+    const display = document.getElementById("display");
+    display.value = eval(display.value);
+}
+function sqr() {
+    const display = document.getElementById("display");
+    display.value = eval(display.value * display.value);
+}
